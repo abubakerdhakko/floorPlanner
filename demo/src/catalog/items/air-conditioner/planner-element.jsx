0bd86d6858df52b7dda5454fcf2cf5ba026fbb34
abubakar siddiquem <fendevdelta@jobesk.com>
2020-09-29 18:33:55 +0500

    rectangleWork

--- demo/src/catalog/items/air-conditioner/planner-element.jsx
+++ demo/src/catalog/items/air-conditioner/planner-element.jsx
@@ -312,36 +312,93 @@ export default {
       },
     },
   },
-  getInitialState: function () {
-    return {
-      size: 60
-    };
-  },
 
   componentDidMount: function () {
-    this.clientY = 0;
+    const el = document.querySelector(".item");
+
+    let isResizing = false;
+
+    el.addEventListener("mousedown", mousedown);
+
+    function mousedown(e) {
+      window.addEventListener("mousemove", mousemove);
+      window.addEventListener("mouseup", mouseup);
+
+      let prevX = e.clientX;
+      let prevY = e.clientY;
+
+      function mousemove(e) {
+        if (!isResizing) {
+          let newX = prevX - e.clientX;
+          let newY = prevY - e.clientY;
+
+          const rect = el.getBoundingClientRect();
+
+          el.style.left = rect.left - newX + "px";
+          el.style.top = rect.top - newY + "px";
+
+          prevX = e.clientX;
+          prevY = e.clientY;
+        }
+      }
+
+      function mouseup() {
+        window.removeEventListener("mousemove", mousemove);
+        window.removeEventListener("mouseup", mouseup);
+      }
+    }
+
+    const resizers = document.querySelectorAll(".resizer");
+    let currentResizer;
+
+    for (let resizer of resizers) {
+      resizer.addEventListener("mousedown", mousedown);
+
+      function mousedown(e) {
+        currentResizer = e.target;
+        isResizing = true;
+
+        let prevX = e.clientX;
+        let prevY = e.clientY;
+
+        window.addEventListener("mousemove", mousemove);
+        window.addEventListener("mouseup", mouseup);
+
+        function mousemove(e) {
+          const rect = el.getBoundingClientRect();
+
+          if (currentResizer.classList.contains("se")) {
+            el.style.width = rect.width - (prevX - e.clientX) + "px";
+            el.style.height = rect.height - (prevY - e.clientY) + "px";
+          } else if (currentResizer.classList.contains("sw")) {
+            el.style.width = rect.width + (prevX - e.clientX) + "px";
+            el.style.height = rect.height - (prevY - e.clientY) + "px";
+            el.style.left = rect.left - (prevX - e.clientX) + "px";
+          } else if (currentResizer.classList.contains("ne")) {
+            el.style.width = rect.width - (prevX - e.clientX) + "px";
+            el.style.height = rect.height + (prevY - e.clientY) + "px";
+            el.style.top = rect.top - (prevY - e.clientY) + "px";
+          } else {
+            el.style.width = rect.width + (prevX - e.clientX) + "px";
+            el.style.height = rect.height + (prevY - e.clientY) + "px";
+            el.style.top = rect.top - (prevY - e.clientY) + "px";
+            el.style.left = rect.left - (prevX - e.clientX) + "px";
+          }
+
+          prevX = e.clientX;
+          prevY = e.clientY;
+        }
+
+        function mouseup() {
+          window.removeEventListener("mousemove", mousemove);
+          window.removeEventListener("mouseup", mouseup);
+          isResizing = false;
+        }
+      }
+    }
   },
 
-  handleMouseDown: function (event) {
-    document.addEventListener('mousemove', this.updateSize);
-    document.addEventListener('mouseup', this.handleMouseUp);
-    this.clientY = event.clientY;
-    console.log('ac')
-  },
 
-  handleMouseUp: function (event) {
-    document.removeEventListener('mousemove', this.updateSize);
-    document.removeEventListener(event.type, this.handleMouseUp);
-  },
-
-  updateSize: function (event) {
-    var delta = event.clientY - this.clientY;
-    this.clientY = event.clientY;
-
-    this.setState({
-      size: _.clamp(this.state.size + delta, 60, 500)
-    })
-  },
   render2D: function (element, layer, scene) {
     let angle = element.rotation + 90;
 
@@ -377,7 +434,7 @@ export default {
           {element.type}
         </text> */}
 
-        <image width={WIDTH} height={DEPTH} style={{
+        <image className="item" width={WIDTH} height={DEPTH} style={{
           stroke: element.selected ? "#0096fd" : "#000",
           strokeWidth: "2px",
           fill: "#84e1ce",
